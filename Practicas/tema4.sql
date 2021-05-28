e07c6972f08f1197806663d2a376d0fae200e0e5
PaCoders <paco.chanivetsanchez@alum.uca.es>
2021-05-28 13:26:15 +0200

    Empezada la practica 8

--- Practicas/tema4.sql
+++ Practicas/tema4.sql
@@ -83,15 +83,64 @@ WHERE tda_num = vnt_tda
 GROUP BY tda_num
 ORDER BY count(*) DESC;
 
+/*
+    8 - Listado de ventas que muestre para cada color la cantidad de artículos vendidos de ese
+color y la cantidad de artículos diferentes. Ordene la consulta por la segunda y tercera
+columna de manera descendente.
+*/
 
+SELECT art_col "Color", sum(vnt_cant) "Cantidad", count(DISTINCT art_num) "Variedad"
+FROM ventas,articulos
+WHERE vnt_art = art_num
+GROUP BY art_col
+ORDER BY Cantidad DESC, Variedad DESC;
 
+/*
+    9 - Listado de artículos vendidos por cada proveedor. El listado mostrará dos columnas: el
+nombre del proveedor y la cantidad de ventas de los artículos de cada proveedor.
+*/
 
+SELECT prv_nom "Nombre del proveedor", count(*) "Cantidad de ventas"
+FROM proveedores, articulos, ventas
+WHERE prv_num = art_prv AND art_num = vnt_art
+GROUP BY prv_num;
 
+/*
+    10 - Listado que muestre la siguiente información de las ventas. Las columnas deben
+mostrar la información y cabeceras tal y como se muestran en el siguiente ejemplo.
+Ordene el resultado por fecha de manera ascendente.
+*/
 
+SELECT CONCAT(clt_nom," ",clt_apell) "Cliente", tda_pob "Tienda", art_nom "Artículo", vnt_cant "Cantidad", vnt_fch "Fecha"
+FROM clientes, tiendas, articulos, ventas
+WHERE clt_num = vnt_clt AND tda_num = vnt_tda AND art_num = vnt_art
+ORDER BY vnt_fch;
 
+/*
+    11 - Listado de artículos que se han vendido tanto en enero como en febrero
+*/
 
+SELECT art_num "Código", art_nom "Nombre", vnt_fch "Fecha de venta"
+FROM articulos JOIN ventas ON art_num = vnt_art
+WHERE MONTH(vnt_fch) = 2 OR MONTH(vnt_fch) = 3;
 
+/*
+    12 - Listado de tiendas que han vendido en febrero menos las tiendas que han vendido en
+enero. Ordenado por código de tienda
+*/
 
+SELECT tda_num "Número", tda_pob "Población"
+FROM tiendas, ventas
+WHERE tda_num = vnt_tda AND MONTH(vnt_fch) = 2 AND MONTH(vnt_fch) != 1
+ORDER BY tda_num;
 
+/*
+    13 - Listado de proveedores que una aquellos que han vendido algún producto en enero
+junto con aquellos de los que se ha vendido algún producto en febrero.
+*/
+
+SELECT prv_num "Código", prv_nom "Nombre"
+FROM proveedores,articulos,ventas
+WHERE prv_num = art_prv AND art_num = vnt_art AND 
 
 
